6161ce9692104ba4dfa3d477313aa928257b9c39
kvendrik <koenvendrik@Koens-MacBook-Pro.local>
2013-10-31 13:52:31 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,43 @@
+Google Fastbutton
+=================
+
+When clicking a element that triggers javascript on a mobile device they wait 300ms to 
+see if the user is going to preform a dubble tap (zooming in). Google fastbutton is a method to avoid 
+this 300ms delay.
+
+This javascript file is basicly the google fastbutton class ready for use.
+
+
+Basic usage
+===========
+
+To start creating fast buttons, first import the javascript.
+
+	<script src='js/google-fastbutton-min.js'></script>
+
+For each button you want to apply the method to, create a new instance specifying the button element and the handler
+
+	new FastButton(document.getElementById('fast_button'), function(){
+		alert('works');
+	});
+
+
+######Little tip
+
+For better control over your fast buttons you could create a function which
+creates a new fast button instance like so:
+
+  Object.prototype.makeFastButton = function(handler){
+
+    if(typeof FastButton !== 'function'){
+      return console.error('Google fastbutton class not found');
+    }
+
+    new FastButton(this, handler);
+
+  };
+
+This way you have better control over what happens when creating new fast buttons without having to 
+touch the google-fastbutton.js file.
+
+
---
+++ google-fastbutton.js
@@ -0,0 +1,85 @@
+(function(){
+
+    this.FastButton = function(element, handler){
+
+      this.element = element;
+      this.handler = handler;
+
+      element.addEventListener('touchstart', this, false);
+      element.addEventListener('click', this, false);
+
+    };
+
+    this.FastButton.prototype.handleEvent = function(e){
+      switch(e.type){
+        case 'touchstart' : this.onTouchStart(e); break;
+        case 'touchmove' : this.onTouchMove(e); break;
+        case 'touchend' : this.onClick(e); break;
+        case 'click' : this.onClick(e); break;
+      }
+    };
+
+    this.FastButton.prototype.onTouchStart = function(e){
+
+      e.stopPropagation();
+
+      this.element.addEventListener('touchend', this, false);
+      this.element.addEventListener('touchmove', this, false);
+
+      this.startX = e.touches[0].clientX;
+      this.startY = e.touches[0].clientY;
+
+    };
+
+    this.FastButton.prototype.onTouchMove = function(e){
+      if(Math.abs(e.touches[0].clientX - this.startX) > 10 ||
+         Math.abs(e.touches[0].clientY - this.startY) > 10){
+          this.reset();
+      }
+    };
+
+    this.FastButton.prototype.onClick = function(e){
+      e.stopPropagation();
+      this.reset();
+      this.handler(e);
+
+      if(e.type == 'touchend'){
+        this.clickbuster.preventGhostClick(this.startX, this.startY);
+      }
+
+    }
+
+    this.FastButton.prototype.reset = function(){
+        this.element.removeEventListener('touchend', this, false);
+        this.element.removeEventListener('touchmove', this, false);
+    };
+
+    this.clickbuster = function(){
+      console.log('init clickbuster');
+    };
+
+    this.clickbuster.preventGhostClick = function(x, y){
+      clickbuster.coordinates.push(x, y);
+      window.setTimeout(google.clickbuster.pop, 2500);
+    };
+
+    this.clickbuster.pop = function(){
+      clickbuster.coordinates.splice(0, 2);
+    };
+
+    this.clickbuster.onClick = function(e){
+      for (var i = 0; i < clickbuster.coordinates.length; i++) {
+        var x = clickbuster.coordinates[i];
+        var y = clickbuster.coordinates[i + 1];
+
+        if(Math.abs(e.clientX - x) < 25 && Math.abs(e.clientY - y) < 25){
+          e.stopPropagation();
+          e.preventDefault();
+        };
+      };
+    };
+
+    document.addEventListener('click', clickbuster.onClick, true);
+    clickbuster.coordinates = [];
+
+})(this);
